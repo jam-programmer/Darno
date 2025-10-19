e68534131979449b8baf133981ea6992f996f7f4
Abbas-Davodkhani <107187833+Abbas-Davodkhani@users.noreply.github.com>
2025-10-20 02:06:52 +0330

    Added : SettingEntity , SettingMap

---
+++ Domain/Entities/SettingEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities;
+
+public class SettingEntity : BaseEntity, IDelete
+{
+    public string Address { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    public string InstagramUrl { get; set; } = string.Empty;
+    public string LinkedInUrl { get; set; } = string.Empty;
+    public string TelegramUrl { get; set; } = string.Empty;
+
+    public string? AboutUs { get; set; }
+
+    public bool IsDelete { get; set; } = false;
+}
---
+++ Infrastructure/Mapping/SettingMap.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Mapping
+{
+    internal sealed class SettingMap : IEntityTypeConfiguration<SettingEntity>
+    {
+        public void Configure(EntityTypeBuilder<SettingEntity> builder)
+        {
+            builder.ToTable("Setting");
+            builder.HasQueryFilter(f => f.IsDelete == false);
+        }
+    }
+}
