54d4b3996fee4093766677f8c9777755a59a5efb
shokooofaaaa <tgshokooofa@gmail.com>
2025-11-18 19:47:34 +0330

    informations of drives

--- EndPoint_Ui/Areas/Admin/Pages/Dashboard/Index.cshtml.cs
+++ EndPoint_Ui/Areas/Admin/Pages/Dashboard/Index.cshtml.cs
@@ -9,5 +9,30 @@ public class IndexModel : PageModel
 {
     public void OnGet()
     {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        foreach (DriveInfo d in allDrives)
+        {
+            System.Diagnostics.Debug.WriteLine( d.Name);
+            System.Diagnostics.Debug.WriteLine("  Drive type:" + d.DriveType);
+            if (d.IsReady)
+            {
+                System.Diagnostics.Debug.WriteLine("  Volume label: "+ d.VolumeLabel);
+                System.Diagnostics.Debug.WriteLine("  File system: "+d.DriveFormat);
+                System.Diagnostics.Debug.WriteLine(
+                    "  Available space to current user:"+
+                    d.AvailableFreeSpace+ "bytes");
+
+                System.Diagnostics.Debug.WriteLine(
+                    "  Total available space:"+
+                    d.TotalFreeSpace+ "bytes");
+
+                System.Diagnostics.Debug.WriteLine(
+                    "  Total size of drive:"+
+                    d.TotalSize+ "bytes");
+            }
+        }
     }
 }
+    
+
