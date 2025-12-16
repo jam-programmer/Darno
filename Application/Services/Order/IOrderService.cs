13b151c3f50592f4ea82841ccba32ff28ed11b8e
shokooofaaaa <tgshokooofa@gmail.com>
2025-12-16 18:28:30 +0330

    display orders

--- Application/Services/Order/IOrderService.cs
+++ Application/Services/Order/IOrderService.cs
@@ -1,13 +1,20 @@
-﻿using Application.DataTransferObject;
+﻿using Application.Common;
+using Application.DataTransferObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.ViewModels;
 
 namespace Application.Services.Order;
 
 public interface IOrderService
 {
     Task InsertOrderAsync(OrderDto order);
+
+    Task<PaginatedList<OrderViewModel>> GetOrdersAsync(Pagination pagination);
+
+   
 }
+
--- Application/Services/Order/OrderService.cs
+++ Application/Services/Order/OrderService.cs
@@ -1,8 +1,11 @@
-﻿using Application.Common.Extension;
+﻿using Application.Common;
+using Application.Common.Extension;
 using Application.Contract;
 using Application.DataTransferObject;
+using Application.ViewModels;
 using Domain.Entities;
 using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Services.Order;
 
@@ -23,4 +26,41 @@ public class OrderService : IOrderService
         await _context.Entity<OrderEntity>().AddAsync(entity);
         await _context.SaveChangesAsync();
     }
+
+   public async Task<PaginatedList<OrderViewModel>> GetOrdersAsync(Pagination pagination)
+
+    {
+        IQueryable<OrderEntity> query = _context.GetQueryable<OrderEntity>();
+        PaginatedList<OrderViewModel> model = new();
+        if (!string.IsNullOrEmpty(pagination!.keyword))
+        {
+            query = query.Where(w => w.Title!.Contains(pagination!.keyword));
+        }
+
+        int count = query.Count().PageCount(pagination!.pageSize);
+        int total = query.Count();
+        var list = await query
+        
+        .Select(o => new OrderViewModel
+        {
+            FullName = o.FullName,
+            Title = o.Title,
+            ProjectType = o.ProjectType
+        })
+        .ToListAsync();
+
+        return new PaginatedList<OrderViewModel>
+        {
+            List = list,
+            TotalItem = total,
+            TotalPage = count,
+            CurrentPage = pagination.currentPage
+        };
+    }
+
+
+
+
+
+
 }
