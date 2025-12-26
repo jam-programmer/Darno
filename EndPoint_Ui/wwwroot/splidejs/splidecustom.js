35ab3812e04c0eb7f3e7cf4e53b5c84b84cb2b57
mohammadreza movahedijam <movahedijam.dev@hotmail.com>
2025-12-26 04:58:51 -0800

    add splidejs

---
+++ EndPoint_Ui/wwwroot/splidejs/splidecustom.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    const mainSlider = new Splide('.splide', {
+        direction: 'ttb',
+        type: 'loop',
+        height: '100%',
+        width: '100%',
+        wheel: true,
+        releaseWheel: true,
+        gap: 0,
+        perPage: 1,
+        autoplay: true,
+        interval: 4000,
+        pauseOnHover: true,
+        speed: 800,
+        arrows: false,
+        pagination: false,
+        drag: true,
+        cover: true,
+
+        breakpoints: {
+            768: {
+                height: '50vh',
+            },
+            480: {
+                height: '40vh',
+            }
+        }
+    });
+
+    mainSlider.mount();
+});
